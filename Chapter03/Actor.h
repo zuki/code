34003e2941fd4f8a3db30881f31866d817c1aa7b
Keiji Suzuki <zuki.ebetsu@gmail.com>
2020-06-15 09:32:02 +0900

    Chapter 3の実行に必要な変更を実施

--- Chapter03/Actor.h
+++ Chapter03/Actor.h
@@ -1,14 +1,14 @@
 // ----------------------------------------------------------------
 // From Game Programming in C++ by Sanjay Madhav
 // Copyright (C) 2017 Sanjay Madhav. All rights reserved.
-// 
+//
 // Released under the BSD License
 // See LICENSE in root directory for full details.
 // ----------------------------------------------------------------
 
 #pragma once
 #include <vector>
-#include "Math.h"
+#include "MathLocal.h"
 #include <cstdint>
 
 class Actor
