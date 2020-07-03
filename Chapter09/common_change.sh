b082f2fd81fb496f9c1a2e52232c52142e731255
Keiji Suzuki <zuki.ebetsu@gmail.com>
2020-07-03 09:20:36 +0900

    章共通の変更をシェル化

---
+++ Chapter09/common_change.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+sed -i .bak -e 's/Math\.h/MathLocal.h/' Actor.h
+sed -i .bak -e 's/Math\.h/MathLocal.h/' AudioSystem.h
+sed -i .bak -e 's/Math\.h/MathLocal.h/' CameraComponent.h
+sed -i .bak -e 's/Math\.h/MathLocal.h/' CircleComponent.h
+sed -i .bak -e 's/Math\.h/MathLocal.h/' Game.h
+sed -i .bak -e 's/Math\.h/MathLocal.h/' Math.cpp
+sed -i .bak -e 's/Math\.h/MathLocal.h/' Mesh.cpp
+sed -i .bak -e 's/Math\.h/MathLocal.h/' Renderer.h
+sed -i .bak -e 's/Math\.h/MathLocal.h/' Shader.h
+sed -i .bak -e 's/Math\.h/MathLocal.h/' SoundEvent.h
+
+sed -i .bak -e 's/setParameterValue/setParameterByName/' SoundEvent.cpp
+sed -i .bak -e 's/getParameterValue/getParameterByName/' SoundEvent.cpp
+sed -i .bak -e 's/getLowLevelSystem/getCoreSystem/' AudioSystem.cpp
+
+mv Math.h MathLocal.h
+
+rm -f *.bak
